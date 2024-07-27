e4f7c50b5b4ab6bd200ea239af3a3c2cbd5e88a9
Matías Mottet <msmottet@gmail.com>
2024-07-27 18:25:36 -0300

    agregramos nuevo problema

---
+++ problema2.py
@@ -0,0 +1,12 @@
+# 1er clase 2do ejemplo
+# Obtención de edades
+
+hermanoBajo = 20
+hermanoAlto = 20
+
+if hermanoBajo > hermanoAlto:
+    print("El hermano bajo es mayor al hermano alto")
+elif hermanoAlto > hermanoBajo:
+    print("El hermano alto es mayor al hermano bajo")
+else:
+    print("Los hermanos tienen la misma edad")
